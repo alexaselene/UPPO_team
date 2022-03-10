9ce5475a9e5b6e907671ee3e762689fbbdf4233b
DianaColin99 <colinochoa.diana@gmail.com>
2022-03-10 00:33:57 -0600

    Se separaron los arreglos de productos, productos personalizados y cursos, además de crear los ciclos para la colocación de los elementos de cada producto y curso en su respectiva plantilla

---
+++ src/js/prod_personalizados.js
@@ -0,0 +1,40 @@
+// Arreglos de los productos personalizados
+let productos_personalizados = [
+    {
+         "Nombre": "Set de mini platos emocionales",
+         "Características": "Dos platos color beige, cara triste y alegre, respectivamente. Sets chicos y medianos",
+         "Material": "Cerámica",
+         "Precio": "$200 mxn",
+         "Stock": "Cuatro unidades"
+    },
+    {
+        "Nombre": "Plato irregular de alta temperatura",
+        "Características": "Un plato de color mármol, diseño rústico, tamaño chico",
+        "Material": "Cerámica, resistente a altas temperaturas desde 650°C - 1000°C",
+        "Precio": "$300 mxn",
+        "Stock": "No disponible"   
+    },
+    {
+        "Nombre": "Mascota personalizada",
+        "Características": "Perros y gatos. Texturas corrugadas y lisas, tamaños medianos y chicos, los colores pueden personalizarse",
+        "Material": "Cerámica",
+        "Precio": "$350 mxn",
+        "Stock": "Disponible"   
+    },
+    {
+        "Nombre": "Recipiente para los pinceles del taller",
+        "Características": "Un recipiente para pinceles, base color beige, tamaño mediano y grande, los colores pueden personalizarse",
+        "Material": "Cerámica",
+        "Precio": "$300 mxn",
+        "Stock": "Disponible" 
+    },
+    {
+        "Nombre": "Florero",
+        "Características": "Un florero color mármol, diseño rústico, tamaño mediano y grande, los colores pueden personalizarse",
+        "Material": "Cerámica",
+        "Precio": "$350 mxn",
+        "Stock": "Disponible" 
+    },
+];
+
+//let jsonProductosPerson= JSON.stringify(productos_personalizados);
